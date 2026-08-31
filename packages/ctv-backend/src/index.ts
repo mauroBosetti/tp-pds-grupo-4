11c0bf84155b84cb6689ec3a92dd09d0de1c9711
Franco <francofumiere321@gmail.com>
2026-08-31 00:09:05 -0300

    Agrego documentación de la API con Swagger

--- packages/ctv-backend/src/index.ts
+++ packages/ctv-backend/src/index.ts
@@ -1,10 +1,17 @@
 import express from 'express'
 import cors from 'cors'
+import YAML from 'yamljs'
+import swaggerUi from 'swagger-ui-express'
 
 const app = express()
 const port = Number(process.env.PORT) || 3000
 
 app.use(cors())
+
+const swaggerDocument = YAML.load('./resources/swagger.yaml');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 app.get('/hola', (_req, res) => {
   res.send('Hola')
