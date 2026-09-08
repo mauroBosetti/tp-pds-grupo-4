b63a8181cb9e1d996b07130639c9b5b7cfaad911
Franco <francofumiere321@gmail.com>
2026-09-08 19:23:07 -0300

    Arreglo campos de la tabla de vuelos

--- packages/api-vuelos/src/db/script.sql
+++ packages/api-vuelos/src/db/script.sql
@@ -1,15 +1,17 @@
 CREATE TABLE IF NOT EXISTS vuelos (
     id_vuelo SERIAL PRIMARY KEY,
+    aerolinea VARCHAR(100) NOT NULL,
     origen varchar(255) NOT NULL,
     destino varchar(255) NOT NULL,
     fecha timestamp NOT NULL,
+    capacidad INT NOT NULL,
     disponibilidad int NOT NULL
 );
 
-INSERT INTO vuelos (origen, destino, fecha, disponibilidad) VALUES
-('Buenos Aires', 'Madrid', '2024-07-01 10:00:00', 100),
-('Madrid', 'Buenos Aires', '2024-07-02 15:00:00', 80),
-('Buenos Aires', 'New York', '2024-07-03 08:00:00', 120),
-('New York', 'Buenos Aires', '2024-07-04 12:00:00', 90),
-('Buenos Aires', 'Paris', '2024-07-05 09:00:00', 110),
-('Paris', 'Buenos Aires', '2024-07-06 14:00:00', 70);
+INSERT INTO vuelos (aerolinea, origen, destino, fecha, capacidad, disponibilidad) VALUES
+('Aerolíneas Argentinas', 'Buenos Aires', 'Madrid', '2027-07-01 10:00:00', 150, 100),
+('Iberia', 'Madrid', 'Buenos Aires', '2027-07-02 15:00:00', 150, 80),
+('Aerolíneas Argentinas', 'Buenos Aires', 'New York', '2027-07-03 08:00:00', 120, 120),
+('Iberia', 'New York', 'Buenos Aires', '2027-07-04 12:00:00', 90, 90),
+('Aerolíneas Argentinas', 'Buenos Aires', 'Paris', '2027-07-05 09:00:00', 110, 110),
+('Iberia', 'Paris', 'Buenos Aires', '2027-07-06 14:00:00', 70, 70);
